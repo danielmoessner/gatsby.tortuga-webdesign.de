19c24f5a678a4cb53238120483a725c2c9f1777b
danielmoessner <github@moesibert.de>
2021-02-09 16:43:37 +0100

    Improved the Animate Component

--- src/components/Animate.jsx
+++ src/components/Animate.jsx
@@ -1,29 +1,37 @@
-import React, { useState, useEffect, useRef } from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useRef, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 function Animate({ animateClass, children }) {
   const [isIntersecting, setIsIntersecting] = useState(false);
-
-  const ref = useRef();
+  const [isLoaded, setIsLoaded] = useState(false);
+  const ref = useRef(null);
 
   useEffect(() => {
     // eslint-disable-next-line no-undef
     const observer = new IntersectionObserver(
       ([entry], obs) => {
         setIsIntersecting(entry.isIntersecting);
-        if (isIntersecting) obs.disconnect();
+        if (entry.isIntersecting && isLoaded) obs.disconnect();
       },
       {
         threshold: 0.01,
-        rootMargin: '0px 0px -200px 0px',
+        rootMargin: '0px 0px -10% 0px',
       }
     );
     if (ref.current) observer.observe(ref.current);
+
+    const setIsLoadedTrue = () => setIsLoaded(true);
+    // eslint-disable-next-line no-undef
+    if (!isLoaded) document.addEventListener('scroll', setIsLoadedTrue);
+    // eslint-disable-next-line no-undef
+    else document.removeEventListener('scroll', setIsLoadedTrue);
+
     // Remove the observer as soon as the component is unmounted
     return () => {
       observer.disconnect();
     };
-  }, [isIntersecting]);
+  }, [isLoaded]);
 
   return (
     <div ref={ref}>
