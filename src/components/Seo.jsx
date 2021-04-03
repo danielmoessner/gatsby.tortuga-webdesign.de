804e9bb7b31dfee004e833b8e8daa4b015912b88
danielmoessner <github@moesibert.de>
2021-04-03 23:54:50 +0200

    udpates

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import PropTypes from 'prop-types';
-import { graphql, StaticQuery } from 'gatsby';
+import { graphql, useStaticQuery } from 'gatsby';
 
-function SEO({ url, title, description, image, isBlogPost }) {
+function SEO({ title, description, image }) {
+  const data = useStaticQuery(
+    graphql`
+      query Favicon {
+        settingsYaml(slug: { eq: "global" }) {
+          favicon {
+            childImageSharp {
+              resize(width: 256, height: 256, cropFocus: CENTER) {
+                src
+              }
+            }
+          }
+        }
+      }
+    `
+  );
+  const favicon = data.settingsYaml.favicon.childImageSharp.resize.src;
+
   return (
     <Helmet htmlAttributes={{ lang: 'de' }}>
       {/* General tags */}
       <title>{title}</title>
       <meta name="description" content={description} />
       {image ? <meta name="image" content={image} /> : null}
-      <link rel="canonical" href={url} />
 
       {/* OpenGraph tags */}
-      {url ? <meta property="og:url" content={url} /> : null}
-      {isBlogPost ? <meta property="og:type" content="article" /> : null}
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       {image ? <meta property="og:image" content={image} /> : null}
@@ -26,49 +40,17 @@ function SEO({ url, title, description, image, isBlogPost }) {
       {image ? <meta name="twitter:image" content={image} /> : null}
 
       {/* Different Favicons */}
-      <StaticQuery
-        query={graphql`
-          query Favicon {
-            settingsYaml(slug: { eq: "global" }) {
-              favicon {
-                childImageSharp {
-                  resize(width: 256, height: 256, cropFocus: CENTER) {
-                    src
-                  }
-                }
-              }
-            }
-          }
-        `}
-        render={(data) => (
-          <>
-            <link
-              rel="icon"
-              type="image/png"
-              href={data.settingsYaml.favicon.childImageSharp.resize.src}
-              sizes="256x256"
-            />
-            <link
-              rel="apple-touch-icon"
-              sizes="256x256"
-              href={data.settingsYaml.favicon.childImageSharp.resize.src}
-            />
-          </>
-        )}
-      />
+      <link rel="icon" type="image/png" href={favicon} sizes="256x256" />
+      <link rel="apple-touch-icon" sizes="256x256" href={favicon} />
     </Helmet>
   );
 }
 
 SEO.defaultProps = {
-  isBlogPost: false,
-  url: '',
   image: '',
 };
 
 SEO.propTypes = {
-  url: PropTypes.string,
-  isBlogPost: PropTypes.bool,
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   image: PropTypes.string,
